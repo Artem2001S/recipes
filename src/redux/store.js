668c93a1060f7906fed41d6d1b11c68debab5dc8
Artem Kazaryan <kazarynarm@mail.ru>
2021-07-26 11:54:02 +0300

    remove local storage saving

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { saveRecipesToLocalStorageMiddleware } from './recipes/middleware/localStorage';
 import { reducers } from './reducers';
 
 export const store = configureStore({
   reducer: reducers,
   devTools: true,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(saveRecipesToLocalStorageMiddleware),
 });
